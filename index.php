3d6f03d27c70b921a6b5780c1b5309619d21ab60
Gabriele-Viola <gabrieleviola50@gmail.com>
2025-02-14 17:17:25 +0100

    add style

--- index.php
+++ index.php
@@ -23,17 +23,40 @@ require_once "./server.php";
 <body>
     <div class="container">
 
-        <h1>My albums</h1>
-        <div class="row row-cols-3">
+        <h1 class="my-4 ">My albums</h1>
+        <div class="add mb-3 ">
+            <form action="" method="POST" class="form-control d-flex justify-content-between py-3 shadow">
+                <div>
+                    <label for="titolo" class="form-label">Titolo</label>
+                    <input type="text" id="titolo" name="titolo" class="form-control">
+                </div>
+                <div>
+                    <label for="autore" class="form-label">Autore</label>
+                    <input type="text" id="autore" name="autore" class="form-control">
+                </div>
+                <div>
+                    <label for="anno" class="form-label">Anno</label>
+                    <input type="number" min="1500" id="anno" name="anno" class="form-control">
+                </div>
+                <div>
+                    <label for="url" class="form-label">URL immagine</label>
+                    <input type="text" id="url" name="url" class="form-control">
+                </div>
+                <div class="d-flex align-items-center">
+                    <button type="submit" class="btn btn-primary">Aggiungi album</button>
+                </div>
+            </form>
+        </div>
+        <div class="row row-cols-3 gy-4 mb-4">
 
             <?php
             foreach ($discs as $disc) {
             ?>
                 <div class="col">
 
-                    <div class="card">
-                        <div class="card-img">
-                            <img src=<?php echo $disc['url']; ?> alt="">
+                    <div class="card h-100 shadow">
+                        <div class="card-img object-fit-cover ratio ratio-1x1 overflow-hidden">
+                            <img class="w-100 object-fit-cover ratio ratio-1x1" src=<?php echo $disc['url']; ?> alt="">
                         </div>
                         <div class="card-body">
                             <h4><?php echo $disc['titolo']; ?></h4>
